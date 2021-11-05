a11390e49fcf126937dc191c81aa6e726ecd3515
lowie <defermelowie@gmail.com>
2021-11-05 17:36:51 +0100

    Add code for opening images

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import os
+from PIL import Image
 import numpy as np
 from matplotlib import pyplot
 from scipy import optimize
@@ -9,3 +10,13 @@ def sigmoid(z):
     Computes the sigmoid of z.
     """
     return 1.0 / (1.0 + np.exp(-z))
+
+
+img = Image.open(
+    './data/Garbage classification/cardboard/cardboard384.jpg')
+
+#print('height: %s, width: %s' % img.size)
+
+(R_chan, G_chan, B_chan) = img.split()
+
+R_chan.show()

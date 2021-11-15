32a3dcf89813e7f49162cfd18494fde6044bbf21
lowie <defermelowie@gmail.com>
2021-11-15 12:01:25 +0100

    Create np array from image

--- src/main.py
+++ src/main.py
@@ -4,10 +4,23 @@ import numpy as np
 from matplotlib import pyplot
 from scipy import optimize
 
+# Constants
+RESCALE_FACTOR = 10
+
 # Open an image, resize and split into channels
 img_og = Image.open(
-    './data/Garbage classification/cardboard/cardboard384.jpg')
-img_res = img_og.resize((img_og.width//5, img_og.height//5))
+    './data/garbage_classification/cardboard/cardboard384.jpg')
+img_res = img_og.resize((img_og.width//RESCALE_FACTOR,
+                        img_og.height//RESCALE_FACTOR))
 (img_R_chan, img_G_chan, img_B_chan) = img_res.split()
 
-print('height: %s, width: %s' % img_res.size)
+print('[INFO] Loaded image: height=%s, width=%s' % img_res.size)
+
+# Combine image channels to np array
+(R_chan, G_chan, B_chan) = (np.asarray(img_R_chan).flatten(),
+                            np.asarray(img_G_chan).flatten(),
+                            np.asarray(img_B_chan).flatten())
+
+input_data = np.concatenate((R_chan, G_chan, B_chan))
+
+print('[INFO] Input data: size=%s' % input_data.shape)

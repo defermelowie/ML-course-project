c7384ce8a1f0f7c4deea64789aa7c035ad24a2c9
lowie <defermelowie@gmail.com>
2021-11-15 12:29:12 +0100

    Wrapped code to load image into function

--- src/main.py
+++ src/main.py
@@ -5,22 +5,32 @@ from matplotlib import pyplot
 from scipy import optimize
 
 # Constants
+VERBOSE = 1
 RESCALE_FACTOR = 10
 
-# Open an image, resize and split into channels
-img_og = Image.open(
-    './data/garbage_classification/cardboard/cardboard384.jpg')
-img_res = img_og.resize((img_og.width//RESCALE_FACTOR,
-                        img_og.height//RESCALE_FACTOR))
-(img_R_chan, img_G_chan, img_B_chan) = img_res.split()
 
-print('[INFO] Loaded image: height=%s, width=%s' % img_res.size)
+def image_path_to_array(path: str, scale_factor: int) -> np.array:
+    # Open an image, resize and split into channels
+    img_og = Image.open(path)
+    img_res = img_og.resize((img_og.width//scale_factor,
+                            img_og.height//scale_factor))
+    (img_R_chan, img_G_chan, img_B_chan) = img_res.split()
 
-# Combine image channels to np array
-(R_chan, G_chan, B_chan) = (np.asarray(img_R_chan).flatten(),
-                            np.asarray(img_G_chan).flatten(),
-                            np.asarray(img_B_chan).flatten())
+    if VERBOSE:
+        print('[INFO] Loaded image: height=%s, width=%s' % img_og.size)
 
-input_data = np.concatenate((R_chan, G_chan, B_chan))
+    # Combine image channels to np array
+    (R_chan, G_chan, B_chan) = (np.asarray(img_R_chan).flatten(),
+                                np.asarray(img_G_chan).flatten(),
+                                np.asarray(img_B_chan).flatten())
+    input_data = np.concatenate((R_chan, G_chan, B_chan))
 
-print('[INFO] Input data: size=%s' % input_data.shape)
+    if VERBOSE:
+        print('[INFO] Generated array: size=%s' % input_data.shape)
+
+    return input_data
+
+
+input_layer = image_path_to_array(
+    './data/garbage_classification/metal/metal45.jpg', RESCALE_FACTOR)
+print(input_layer)

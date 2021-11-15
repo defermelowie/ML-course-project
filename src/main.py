327e78a384338d88132ae7f0622da1745f3032cf
lowie <defermelowie@gmail.com>
2021-11-15 11:04:52 +0100

    Resize img

--- src/main.py
+++ src/main.py
@@ -4,19 +4,10 @@ import numpy as np
 from matplotlib import pyplot
 from scipy import optimize
 
-
-def sigmoid(z):
-    """
-    Computes the sigmoid of z.
-    """
-    return 1.0 / (1.0 + np.exp(-z))
-
-
-img = Image.open(
+# Open an image, resize and split into channels
+img_og = Image.open(
     './data/Garbage classification/cardboard/cardboard384.jpg')
+img_res = img_og.resize((img_og.width//5, img_og.height//5))
+(img_R_chan, img_G_chan, img_B_chan) = img_res.split()
 
-#print('height: %s, width: %s' % img.size)
-
-(R_chan, G_chan, B_chan) = img.split()
-
-R_chan.show()
+print('height: %s, width: %s' % img_res.size)

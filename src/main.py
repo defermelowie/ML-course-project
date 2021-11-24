f808093f88c26d763daff3079d5286d31ecf0968
lowie <defermelowie@gmail.com>
2021-11-24 15:27:41 +0100

    Image to arr

--- src/main.py
+++ src/main.py
@@ -1,23 +1,22 @@
-import os
 from PIL import Image
 import numpy as np
-from matplotlib import pyplot
-from scipy import optimize
 
 # Constants
 VERBOSE = 1
-RESCALE_FACTOR = 10
+IMG_RESCALE_FACTOR = 15
 
 
 def image_path_to_array(path: str, scale_factor: int) -> np.array:
     # Open an image, resize and split into channels
     img_og = Image.open(path)
     img_res = img_og.resize((img_og.width//scale_factor,
-                            img_og.height//scale_factor))
+                            img_og.height//scale_factor),
+                            resample=Image.ANTIALIAS)
     (img_R_chan, img_G_chan, img_B_chan) = img_res.split()
 
     if VERBOSE:
         print('[INFO] Loaded image: height=%s, width=%s' % img_og.size)
+        print('[INFO] Resized image: height=%s, width=%s' % img_res.size)
 
     # Combine image channels to np array
     (R_chan, G_chan, B_chan) = (np.asarray(img_R_chan).flatten(),
@@ -32,5 +31,5 @@ def image_path_to_array(path: str, scale_factor: int) -> np.array:
 
 
 input_layer = image_path_to_array(
-    './data/garbage_classification/metal/metal45.jpg', RESCALE_FACTOR)
+    './data/garbage_classification/metal/metal45.jpg', IMG_RESCALE_FACTOR)
 print(input_layer)
